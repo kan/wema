01001fdcf0a1ef8330f6154f4da9fd3078b10012
Kan Fushihara <kan.fushihara@gmail.com>
2012-09-09 22:25:20 +0900

    post, closeボタンを一応動作するようにした
    - 付箋div要素のhtml片を分離
    - 付箋ドラッグのhandlerを分離して入力欄と付箋で使い回し
    - 入力欄を閉じる際にtextareaの内容をクリア

--- js/wema.js
+++ js/wema.js
@@ -1,16 +1,38 @@
 /* wema v3.0 - https://github.com/kan/wema */
 (function($){
-    var inputTag = $('<div style="position:absolute; padding: 5px; border: solid 1px #000"' +
-                          'draggable="true" />');
-    inputTag.bind('dragend', function(e) {
-      var rect = e.target.getBoundingClientRect();
-      $(this).css('left', e.pageX)
-             .css('top',  e.pageY - rect.height);
-    })
-      .append('<textarea id="tag_text" style="width: 20em; height: 4em"></textarea><br />')
-      .append('<input type="button" value="close" />')
-      .append('<input type="button" value="post" />')
-      .hide();
+
+    var TAGDIV = '<div style="position:absolute; padding: 5px; border: solid 1px #000"' +
+                      'draggable="true" />';
+
+    var tagDragHandler = function(e) {
+        var rect = e.target.getBoundingClientRect();
+        $(this).css('left', e.pageX)
+               .css('top',  e.pageY - rect.height);
+    };
+
+    var inputTag = $(TAGDIV);
+
+    var tagText = $('<textarea style="width: 20em; height: 4em"></textarea>');
+
+    var closeBtn = $('<input type="button" value="close" />')
+      .bind('click', function(e) { tagText.val(''); inputTag.hide() });
+
+    var postBtn = $('<input type="button" value="post" />')
+      .bind('click', function(e) {
+        var tag = $(TAGDIV).text(tagText.val())
+                           .css('left', inputTag.css('left'))
+                           .css('top',  inputTag.css('top'))
+                           .bind('dragend', tagDragHandler);
+        $('body').append(tag);
+        tagText.val('');
+        inputTag.hide();
+      });
+
+    inputTag.bind('dragend', tagDragHandler)
+            .append(tagText).append('<br />')
+            .append(closeBtn)
+            .append(postBtn)
+            .hide();
 
     $('body').append(inputTag)
              .bind('dblclick', function(e) {
