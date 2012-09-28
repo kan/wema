ea852ca1cf5d3977baacbc037e6cd9612d68bcbf
Kan Fushihara <kan.fushihara@gmail.com>
2012-09-28 15:09:25 +0900

    storageの実装を分離した

---
+++ js/wema-storage-local.js
@@ -0,0 +1,57 @@
+var WemaStorage = {
+    tagNumber: 0,
+    tagDomain: 'net.fushihara.wema.tag',
+
+    generateTagHandler: function(tagInfo) { },
+
+    saveTag: function(text, left, top) {
+        var tagStr = JSON.stringify({'text': text,                                                            
+            'tagId': WemaStorage.tagDomain+WemaStorage.tagNumber,
+            'left': left,
+            'top': top});
+
+        localStorage.setItem(WemaStorage.tagDomain+WemaStorage.tagNumber, tagStr);
+        WemaStorage.generateTagHandler(JSON.parse(tagStr));
+        WemaStorage.tagNumber++;
+    },
+
+    updateTag: function(id, text, left, top) {
+        alert(id, text);
+        var tagStr = JSON.stringify({'text': text,                                                            
+            'left': left,
+            'top': top});
+
+        localStorage.setItem(id, tagStr);
+    },
+
+    clearAllTags: function() {
+        var i = 0;
+        while(true) {
+            var tagStr = localStorage.getItem(WemaStorage.tagDomain + i);
+            if (tagStr) {
+                localStorage.removeItem(WemaStorage.tagDomain + i);
+                i++;
+            } else {
+                break;
+            }
+        }
+        WemaStorage.tagNumber = 0; // reset
+    },
+
+    loadAllTags: function() {
+        WemaStorage.tagNumber = 0;
+
+        while(true) {
+            var tagStr = localStorage.getItem(WemaStorage.tagDomain + WemaStorage.tagNumber);                                                                 
+            if (tagStr) {
+                var tagInfo = JSON.parse(tagStr);
+                tagInfo.tagId = WemaStorage.tagDomain + WemaStorage.tagNumber;
+                WemaStorage.generateTagHandler(tagInfo);
+                WemaStorage.tagNumber++;
+            } else {
+                break;
+            }
+        }
+    },
+};
+
--- js/wema.js
+++ js/wema.js
@@ -3,67 +3,42 @@
 
     var TAGDIV = '<div style="position:absolute; padding: 5px; border: solid 1px #000"' +
                       'draggable="true" />';
-    
-    var TAGDOMAIN = 'net.fushihara.wema.tag';
-
-    var tagNumber = 0;
 
     var tagDragHandler = function(e) {
         var rect = e.target.getBoundingClientRect();
         $(this).css('left', e.pageX)
                .css('top',  e.pageY - rect.height);
         if ($(this).data('tagId')) {
-          var tagStr = JSON.stringify({ 'text': $(this).text(),
-                                        'left': $(this).css('left'),
-                                        'top' : $(this).css('top') });
-
-          localStorage.setItem($(this).data('tagId'), tagStr);
+          WemaStorage.updateTag($(this).data('tagId'), $(this).text(), $(this).css('left'), $(this).css('top'));
         }
+    };
+
+    WemaStorage.generateTagHandler = function(tagInfo) {
+        var tag = $(TAGDIV).text(tagInfo.text)
+                           .css('left', tagInfo.left)
+                           .css('top',  tagInfo.top)
+                           .data('tagId', tagInfo.tagId)
+                           .addClass('wema-tag')
+                           .bind('dragend', tagDragHandler);
+        $('body').append(tag);
     };
 
     var inputTag = $(TAGDIV);
 
     var tagText = $('<textarea style="width: 20em; height: 4em"></textarea>');
 
-    function saveTag() {
-        var tagStr = JSON.stringify({ 'text': tagText.val(),
-          'left': inputTag.css('left'),
-          'top': inputTag.css('top') });
-
-        localStorage.setItem(TAGDOMAIN+tagNumber, tagStr);
-        tagNumber++;
-    }
-
     var closeBtn = $('<input type="button" value="close" />')
       .bind('click', function(e) { tagText.val(''); inputTag.hide() });
 
     var clearBtn = $('<input type="button" value="clear all tags" />')
       .bind('click', function(e) {
-        var i = 0;
-
-        while(true) {
-          var tagStr = localStorage.getItem(TAGDOMAIN + i);
-          if (tagStr) {
-            localStorage.removeItem(TAGDOMAIN + i);
-            i++;
-          } else {
-            break;
-          }
-        }
-        tagNumber = 0; // reset
+        WemaStorage.clearAllTags();
         $('div.wema-tag').each(function(index) { $(this).remove() });
       });
 
     var postBtn = $('<input type="button" value="post" />')
       .bind('click', function(e) {
-        var tag = $(TAGDIV).text(tagText.val())
-                           .css('left', inputTag.css('left'))
-                           .css('top',  inputTag.css('top'))
-                           .data('tagId', TAGDOMAIN + tagNumber)
-                           .addClass('wema-tag')
-                           .bind('dragend', tagDragHandler);
-        $('body').append(tag);
-        saveTag();
+        WemaStorage.saveTag(tagText.val(), inputTag.css('left'), inputTag.css('top'));
         tagText.val('');
         inputTag.hide();
       });
@@ -83,22 +58,7 @@
     });
 
     // load tags
-    while(true) {
-      var tagStr = localStorage.getItem(TAGDOMAIN + tagNumber);
-      if (tagStr) {
-        var tagInfo = JSON.parse(tagStr);
-        var tag = $(TAGDIV).text(tagInfo.text)
-                           .css('left', tagInfo.left)
-                           .css('top',  tagInfo.top)
-                           .data('tagId', TAGDOMAIN + tagNumber)
-                           .addClass('wema-tag')
-                           .bind('dragend', tagDragHandler);
-        $('body').append(tag);
-        tagNumber++;
-      } else {
-        break;
-      }
-    }
+    WemaStorage.loadAllTags();
 
 }(Zepto || jQuery));
 
